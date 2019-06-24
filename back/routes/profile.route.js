df122c8c7df4b7be00b4508f2bb16fb7fb9c9174
PatrickNirina002 <pjsranaivo@gmail.com>
2019-06-24 15:40:29 +0300

    test

---
+++ back/routes/profile.route.js
@@ -0,0 +1,10 @@
+module.exports = (app) => {
+
+    const pers = require('../controller/profile.controller');
+    app.post('/profil', pers.create);
+    app.get('/profil', pers.findAll);
+    app.get('/profil/:profilId', pers.findOne);
+    app.get('/user/:photo_profil', pers.lireImage);
+    // app.put('/profil/:profilId', profil.update);
+ 
+}
